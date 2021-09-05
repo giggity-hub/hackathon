e3801876ace5e69442a0e57c35da41325ad8d471
Zeusmaxhallo <max@tbrockmann.de>
2021-09-05 14:35:18 +0200

    Header

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
+
+
 <script>
 	export let name;
+  import Header from "./Header.svelte"
 </script>
 
 
 <svelte:head>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
 </svelte:head>
-<div class="container">
-	hello world
-  </div>
+
+
+
+<Header/>
